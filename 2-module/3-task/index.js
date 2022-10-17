c8c5591343500f3f38ed0110685e6e845938a08e
Байков Роман <baikov@infoculture.ru>
2022-10-17 21:42:08 +0300

    второй модуль

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,24 @@
+
 let calculator = {
-  // ваш код
-};
+  sum: function() {
+    return this.a + this.b;
+  },
+
+  mul: function() {
+    return this.a * this.b;
+  },
+
+  read: function(a, b) {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
+}
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
